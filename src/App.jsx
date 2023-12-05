98f85ef852edb44eea10fc2701da7a64dda9e7d9
AxelJaBo <axel.910921@gmail.com>
2023-12-04 20:50:03 -0800

    Outlet: nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import { BlogPost } from "./BlogPost";
 import { Menu } from "./Menu";
 
 function App() {
-
-
   return (
     <>
       <HashRouter>
-        <Menu/>
+        <Menu />
+
         <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/blog" element={<BlogPage />}/>
-          <Route path="/blog/:slug" element={<BlogPost />}/>
-          <Route path="/profile" element={<ProfilePage />}/>
-          <Route path="*" element={<p>Not found</p>}/>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/blog" element={<BlogPage />}>
+            <Route path=":slug" element={<BlogPost />} />
+          </Route>
+
+          <Route path="/profile" element={<ProfilePage />} />
+
+          <Route path="*" element={<p>Not found</p>} />
         </Routes>
       </HashRouter>
     </>
-  )
+  );
 }
 
 export default App
