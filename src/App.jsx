ec3c674a7af99e7c8d41f1b4e963345242cf627d
AxelJaBo <axel.910921@gmail.com>
2023-12-05 20:47:33 -0800

    useAuth: login y logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { BlogPage } from "./BlogPage";
-import { HomePage } from "./HomePage";
-import { ProfilePage } from "./ProfilePage";
-import { BlogPost } from "./BlogPost";
 import { Menu } from "./Menu";
+import { AuthProvider } from "./auth";
+import { HomePage } from "./HomePage";
+import { BlogPage } from "./BlogPage";
+import { BlogPost } from "./BlogPost";
+import { ProfilePage } from "./ProfilePage";
+import { LoginPage } from "./LoginPage";
+import { LogoutPage } from "./LogoutPage";
 
 function App() {
   return (
     <>
       <HashRouter>
-        <Menu />
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-
-          <Route path="/blog" element={<BlogPage />}>
-            <Route path=":slug" element={<BlogPost />} />
-          </Route>
-
-          <Route path="/profile" element={<ProfilePage />} />
-
-          <Route path="*" element={<p>Not found</p>} />
-        </Routes>
+        <AuthProvider>
+          <Menu />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/blog" element={<BlogPage />}>
+              <Route path=":slug" element={<BlogPost />} />
+            </Route>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/logout" element={<LogoutPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<p>Not found</p>} />
+          </Routes>
+        </AuthProvider>
       </HashRouter>
     </>
   );
